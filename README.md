7f8701c52e8f74df5828dd3d26fed079f9fcec91
misostack <misostack.com@gmail.com>
2024-02-01 09:18:30 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# ReactJS Tutorial 2024
+
+- Made with love from [nextjsvietnam.com](https://nextjsvietnam.com/categories/reactjs-tutorial/)
+
+> Some notes:
+
+- ReactJS Version: ^18.2
+- Language: Typescript
+- Bundler Tool: vite
+- Test Framework: vitest
---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import reactTutorialLogo from "./assets/reactjs-tutorial.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a
+          href="https://nextjsvietnam.com/categories/reactjs-tutorial/"
+          target="_blank"
+        >
+          <img
+            src={reactTutorialLogo}
+            className="logo react"
+            alt="React logo"
+          />
+        </a>
+      </div>
+      <h1>ReactJS Tutorial 2024</h1>
+      <div>**Target**</div>
+      <p>Be able to use ReactJS efficiently in your real world projects</p>
+    </>
+  );
+}
+
+export default App;
